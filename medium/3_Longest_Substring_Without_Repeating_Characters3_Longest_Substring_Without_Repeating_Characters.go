215d1feaedf750402c223c88c540325723b1694a
Clinton Mekwunye <mekwunyeclinton22@gmail.com>
2023-02-07 03:48:53 +0100

    3_Longest_Substring_Without_Repeating_Characters explanation

--- medium/3_Longest_Substring_Without_Repeating_Characters3_Longest_Substring_Without_Repeating_Characters.go
+++ medium/3_Longest_Substring_Without_Repeating_Characters3_Longest_Substring_Without_Repeating_Characters.go
@@ -32,6 +32,43 @@ import (
 	"fmt"
 )
 
+/* EXPLANATION
+IDEA
+- The idea utilizes the sliding window concept
+- we will create a map/hashset to store the characters of the string
+- we can loop through the characters using the index.
+	if we find the given character in the map. We remove characters of
+	the string from the dictionary until the character isn't in the map.
+	this is done by keeping track of the first member of the dict by
+	an integer l.
+	else, we add the character to the map and check if the current length
+	is the maximum.
+
+CODE
+- compute the function for finding max of 2 integers
+- Initialize the map called charSet, it uses byte as key and
+bool as value
+
+*NOTE: byte as key is done because golang returns byte when you
+* get a character by its index from a string. Since each character
+* has a unique byte there is no need to change it to string.
+*NOTE: It is imperative to use bool data type for the value: it helps
+* with the condition later on.
+
+- Initialize l: an integer that tracks the current window (pointer)
+position
+- Initialize maxLength: an integer that tracks the maximum length
+
+- Run a for loop on the string keeping/using the index
+	- run a while loop (For, golang doesn't have a while loop) with
+	condition if the current character is present in the map.
+	- If true, remove the element s[l] from the dict and increment l.
+	- else, add the character to the map and check if the current
+	length is the max length.
+
+- return maxLength
+*/
+
 func max(a, b int) int {
 	if a > b {
 		return a
